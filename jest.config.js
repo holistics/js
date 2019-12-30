a13b172f292bdbee8e4943c78cf6bb1128cedbe2
Hoang Do <me@kaid.sh>
2019-12-30 17:06:24 +0700

    force 100% coverage :pray:

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'packages/**/src/**/*.js',
+    '!**/dist/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
