361cc271a4a4bd30a8cb0a8acee71ea66b32db3b
Hoang Do <me@kaid.sh>
2020-01-03 11:34:32 +0700

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 
@@ -30,13 +30,6 @@ export default (pkgName) => ([
       format: 'cjs',
       exports: 'named',
     },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      resolve(),
-      commonjs(),
-    ],
   },
   {
     input: 'src/index.js',
@@ -44,12 +37,5 @@ export default (pkgName) => ([
       file: `./dist/${pkgName}.esm.js`,
       format: 'es',
     },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-      }),
-      resolve(),
-      commonjs(),
-    ],
   },
 ]);
