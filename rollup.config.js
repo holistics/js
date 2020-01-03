3000408ea8909fae06ced5b088ce4a979db66b5a
Hoang Do <me@kaid.sh>
2020-01-03 10:35:21 +0700

    resolve & commonjs plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import babel from 'rollup-plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 
