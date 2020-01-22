6d90f135e5f39c982c78163e13dcfcff8e17b3cc
datbui <scott.bui@holistics.io>
2020-01-22 17:09:12 +0700

    do not publish compiled pkg

--- packages/date-parser/cli/build.js
+++ packages/date-parser/cli/build.js
@@ -2,4 +2,4 @@ const { exec } = require('pkg');
 const pjson = require('../package.json');
 
 const cliVersion = pjson.version.replace(/\./g, '_');
-exec(['./cli/index.js', '--targets=node12-linux-x64', `--output=./dist/date_parser_${cliVersion}`]);
+exec(['./cli/index.js', '--targets=node12-linux-x64', `--output=./out/date_parser_${cliVersion}`]);
