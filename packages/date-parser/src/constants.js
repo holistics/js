39f87876c21815bb892298dffb6e9691e81e3ef3
huyphung1602 <huy.phung.sw@gmail.com>
2021-05-06 19:01:32 +0700

    Add hash to remapping weekday to wsd values

--- packages/date-parser/src/constants.js
+++ packages/date-parser/src/constants.js
@@ -25,6 +25,23 @@ export const WEEKDAYS = {
   sunday: 6,
 };
 
+export const WSD_REMAPPING = {
+  sun: 0,
+  sunday: 0,
+  mon: 1,
+  monday: 1,
+  tue: 2,
+  tuesday: 2,
+  wed: 3,
+  wednesday: 3,
+  thu: 4,
+  thursday: 4,
+  fri: 5,
+  friday: 5,
+  sat: 6,
+  saturday: 6,
+};
+
 /**
  * @enum {String}
  */
