4104be74d82d0cfc4e24cba81494849853e5fcf2
Huy Vu <khanhhuy.dev@gmail.com>
2021-08-11 13:08:36 +0700

    migrate constants parser to V2 API

--- packages/date-parser/src/parsers/constants.js
+++ packages/date-parser/src/parsers/constants.js
@@ -1,39 +1,37 @@
-import Chrono from 'chrono-node';
 import dateStructFromDate from '../helpers/dateStructFromDate';
 
-const parser = new Chrono.Parser();
+const parser = {};
 
 parser.pattern = () => {
   return new RegExp('(beginning|now)', 'i');
 };
 
 /**
- * @param {String} text
- * @param {Date} ref
+ * @param {Chrono.ParsingContext} context
  * @param {Array} match
  */
-parser.extract = (text, ref, match) => {
+parser.extract = (context, match) => {
   const date = match[1].toLowerCase();
 
   let start;
   let end;
   /* istanbul ignore else */
   if (date === 'beginning') {
-    start = { year: 1970, month: 1, day: 1, hour: 0, minute: 0, second: 0 };
+    start = {
+      year: 1970, month: 1, day: 1, hour: 0, minute: 0, second: 0,
+    };
   } else if (date === 'now') {
-    const refDateStruct = dateStructFromDate(ref);
+    const refDateStruct = dateStructFromDate(context.reference.instant);
     const nowDateStruct = { ...refDateStruct, month: refDateStruct.month + 1 };
     start = nowDateStruct;
   }
 
-  return new Chrono.ParsedResult({
-    ref,
-    text: match[0],
-    index: match.index,
-    tags: { constantParser: true },
+  return context.createParsingResult(
+    match.index,
+    match[0],
     start,
     end,
-  });
+  );
 };
 
 export default parser;
