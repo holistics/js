57da363c52168c7256d9294351dbd095ec8c78f4
Huy Vu <khanhhuy.dev@gmail.com>
2021-08-11 16:48:53 +0700

    migrate to chrono-v2

--- packages/date-parser/src/parsers/guards/ambiguousWeekday.js
+++ packages/date-parser/src/parsers/guards/ambiguousWeekday.js
@@ -1,19 +1,17 @@
-import Chrono from 'chrono-node';
 import { WEEKDAYS_MAP } from '../../constants';
 import { ParseError } from '../../errors';
 
-const guard = new Chrono.Parser();
+const guard = {};
 
 guard.pattern = () => {
   return new RegExp(`^\\s*((?:last|this|next) )?(${Object.keys(WEEKDAYS_MAP).join('|')})\\s*$`, 'i');
 };
 
 /**
- * @param {String} text
- * @param {Date} ref
+ * @param {ParsingContext} context
  * @param {Array} match
  */
-guard.extract = (text, ref, match) => {
+guard.extract = (context, match) => {
   const modifier = (match[1] || 'last/this/next').trim();
   const weekday = match[2];
   throw new ParseError(`"${match[0]}" is ambiguous. Please try "${weekday} ${modifier} week" instead`);
