eb2ab222967894126999bc342b43e4a89e17e119
Huy Vu <khanhhuy.dev@gmail.com>
2021-08-13 12:41:38 +0700

    initalize dayjs with tz plugin

--- packages/date-parser/src/initializers/dayjs.js
+++ packages/date-parser/src/initializers/dayjs.js
@@ -3,10 +3,12 @@ import en from 'dayjs/locale/en';
 import utcPlugin from 'dayjs/plugin/utc';
 import weekdayPlugin from 'dayjs/plugin/weekday';
 import quarterOfYearPlugin from 'dayjs/plugin/quarterOfYear';
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.locale({
   ...en,
 });
 dayjs.extend(weekdayPlugin);
 dayjs.extend(utcPlugin);
+dayjs.extend(timezone);
 dayjs.extend(quarterOfYearPlugin);
