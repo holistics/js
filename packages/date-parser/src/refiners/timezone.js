b46e355ebc7ca9fca1c9fe76279a3c12d339befb
Huy Vu <khanhhuy.dev@gmail.com>
2021-08-13 12:31:17 +0700

    rework timezone implier

--- packages/date-parser/src/refiners/timezone.js
+++ packages/date-parser/src/refiners/timezone.js
@@ -1,23 +1,30 @@
-import Chrono from 'chrono-node';
+import dayjs from 'dayjs';
+import { isNil } from 'lodash';
 
 /**
  *
  * @param {Chrono.ParsedComponents} parsedComponent
  */
-const implyTimezone = (parsedComponent, timezoneOffset) => {
-  parsedComponent.imply('timezoneOffset', (timezoneOffset || 0));
+const implyTimezone = (parsedComponent, timezone) => {
+  const offset = dayjs.tz(parsedComponent.date(), timezone).utcOffset();
+  parsedComponent.imply('timezoneOffset', (offset || 0));
 };
 
-const timezoneRefiner = new Chrono.Refiner();
-timezoneRefiner.refine = (text, results, { timezoneOffset }) => {
-  /**
-   *
-   * @param {Chrono.ParsedResult} res
-   */
+const timezoneRefiner = {};
+
+/**
+ * @param {ParsingContext} context
+ * @param {ParsedResult[]} results
+ * @return {ParsedResult[]}
+ */
+timezoneRefiner.refine = (context, results) => {
+  const { timezone } = context.option;
+
   return results.map(res => {
-    /* istanbul ignore else */
-    if (res.start) implyTimezone(res.start, timezoneOffset);
-    if (res.end) implyTimezone(res.end, timezoneOffset);
+    // If input is an ISO string, then timezone is already extracted from the input,
+    // thus timezoneOffset is not nil
+    if (isNil(res.start.get('timezoneOffset')) && res.start) implyTimezone(res.start, timezone);
+    if (isNil(res.end.get('timezoneOffset')) && res.end) implyTimezone(res.end, timezone);
     return res;
   });
 };
