b29ea9a75c103a4e71e351666853f24c2827f245
Huy Vu <khanhhuy.dev@gmail.com>
2021-09-26 21:59:59 +0700

    new index that support versioning parser

---
+++ packages/date-parser/src/index.js
@@ -0,0 +1,41 @@
+import { parse as parseV1 } from './dateParserV1';
+import { parse as parseV2 } from './dateParserV2';
+import { WEEKDAYS, OUTPUT_TYPES } from './constants';
+import Errors from './errors';
+
+/**
+ * Parse the given date string into Chrono.ParsedResult
+ * @param {String} str The date string to parse
+ * @param {String|Date} ref Reference date
+ * @param {Object} options
+ * @param {Number} options.timezoneOffset Timezone offset in minutes
+ * @param {OUTPUT_TYPES} options.output Type of the output dates
+ * @param {Number} weekStartDay The weekday chosen to be the start of a week. See WEEKDAYS constant for possible values
+ * @param {Number} parserVersion V1 supports timezone offset while V2 supports timezone region only. Use for backward compatabiblity
+ * @param {String} timezoneRegion timezone region, only available in V2 parser
+ * @return {ChronoNode.ParsedResult|Array}
+ */
+export const parse = (str, ref, {
+  timezoneOffset = 0,
+  timezoneRegion = 'Etc/UTC',
+  output = OUTPUT_TYPES.parsed_component,
+  weekStartDay = WEEKDAYS.Monday,
+  parserVersion = 1,
+} = {}) => {
+  if (parserVersion === 2) {
+    return parseV2(str, ref, { timezoneRegion, output, weekStartDay });
+  }
+
+  // V1 parser that supports timezone offset
+  return parseV1(str, ref, { timezoneOffset, output, weekStartDay });
+};
+
+export { WEEKDAYS, OUTPUT_TYPES } from './constants';
+export { default as Errors } from './errors';
+
+export default {
+  parse,
+  WEEKDAYS,
+  OUTPUT_TYPES,
+  Errors,
+};
