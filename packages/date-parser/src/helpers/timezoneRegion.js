d332abe7fa5caac556d6327c289bb7aae61ac8fb
Huy Vu <khanhhuy.dev@gmail.com>
2021-10-04 11:36:48 +0700

    Fix bug invalid timezone not throw error

--- packages/date-parser/src/helpers/timezoneRegion.js
+++ packages/date-parser/src/helpers/timezoneRegion.js
@@ -7,9 +7,12 @@ class TimezoneRegion {
    */
   constructor (region) {
     try {
-      DateTime.fromISO('2021-05-28T09:10:23', { zone: region });
+      const date = DateTime.fromISO('2021-05-28T09:10:23', { zone: region });
+      if (!date.isValid) {
+        throw new Error(date.invalidReason);
+      }
     } catch (err) {
-      throw new InputError(`Invalid timezone region: ${err.message}`);
+      throw new InputError(`Invalid timezone region: ${region}, ${err.message}`);
     }
     this.region = region;
   }
