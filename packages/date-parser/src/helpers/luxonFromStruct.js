2af1f710e20d34dc9386a31c5bad88483e159308
Huy Vu <khanhhuy.dev@gmail.com>
2021-10-27 21:52:20 +0700

    some fix

--- packages/date-parser/src/helpers/luxonFromStruct.js
+++ packages/date-parser/src/helpers/luxonFromStruct.js
@@ -1,8 +1,8 @@
 import { DateTime } from 'luxon';
 
 export default ({
-  year, month, day, hour, minute, second, millisecond,
-}, timezone) => {
+  year, month, day, hour, minute, second, millisecond, timezone,
+}) => {
   return DateTime.fromObject({
     year,
     month, // luxon's month starts at 1, same as our date struct
