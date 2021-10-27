880d155278ab6b5946dbc14da625108f4d0bfaf0
Huy Vu <khanhhuy.dev@gmail.com>
2021-10-27 21:58:52 +0700

    remove unused codes in timezone v3

--- packages/date-parser/src/refiners/v3/timezone.js
+++ packages/date-parser/src/refiners/v3/timezone.js
@@ -1,13 +1,5 @@
 import Chrono from 'chrono-node';
 
-/**
- *
- * @param {Chrono.ParsedComponents} parsedComponent
- */
-const implyTimezoneOffset = (parsedComponent, timezoneOffset) => {
-  parsedComponent.imply('timezoneOffset', (timezoneOffset || 0));
-};
-
 const implyTimezoneRegion = (parsedComponent, timezone) => {
   if (!parsedComponent.get('timezone')) {
     parsedComponent.imply('timezone', timezone);
@@ -15,19 +7,14 @@ const implyTimezoneRegion = (parsedComponent, timezone) => {
 };
 
 const timezoneRefiner = new Chrono.Refiner();
-timezoneRefiner.refine = (text, results, { timezoneOffset, timezone }) => {
+timezoneRefiner.refine = (text, results, { timezone }) => {
   /**
    *
    * @param {Chrono.ParsedResult} res
    */
   return results.map(res => {
-    if (timezone) {
-      implyTimezoneRegion(res.start, timezone);
-      if (res.end) implyTimezoneRegion(res.end, timezone);
-    } else {
-      implyTimezoneOffset(res.start, timezoneOffset);
-      if (res.end) implyTimezoneOffset(res.end, timezoneOffset);
-    }
+    implyTimezoneRegion(res.start, timezone);
+    if (res.end) implyTimezoneRegion(res.end, timezone);
 
     return res;
   });
