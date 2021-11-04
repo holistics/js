fa9c851361ae83a5ee7d7eba72fd36f68789699e
Huy Vu <khanhhuy.dev@gmail.com>
2021-11-04 18:17:29 +0700

    use utc to avoid browser issue

--- packages/date-parser/src/helpers/luxonFromStruct.js
+++ packages/date-parser/src/helpers/luxonFromStruct.js
@@ -4,21 +4,19 @@ import { ParseError } from '../errors';
 export default ({
   year, month, day, hour, minute, second, millisecond, timezone,
 }) => {
-  const datetime = DateTime.fromObject({
+  const datetime = DateTime.utc(
     year,
-    month, // luxon's month starts at 1, same as our date struct
+    month,
     day,
     hour,
     minute,
     second,
     millisecond,
-  }, {
-    zone: timezone,
-  });
+  );
 
   if (!datetime.isValid) {
     throw new ParseError(`${datetime.invalidReason}: ${datetime.invalidExplanation}`);
   }
 
-  return datetime;
+  return datetime.setZone(timezone, { keepLocalTime: true });
 };
