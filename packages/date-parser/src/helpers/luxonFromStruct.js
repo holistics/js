126de1778667cba710e2f5fe6f40cce2c5d6d6f0
Huy Vu <khanhhuy.dev@gmail.com>
2021-11-04 18:19:01 +0700

    add comment

--- packages/date-parser/src/helpers/luxonFromStruct.js
+++ packages/date-parser/src/helpers/luxonFromStruct.js
@@ -4,6 +4,8 @@ import { ParseError } from '../errors';
 export default ({
   year, month, day, hour, minute, second, millisecond, timezone,
 }) => {
+  // Warning: don't use fromObject here because its behavior is inconsistent in the browser
+  // See https://github.com/holistics/js/pull/15
   const datetime = DateTime.utc(
     year,
     month,
