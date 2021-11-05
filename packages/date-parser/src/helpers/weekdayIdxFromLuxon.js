471902b43510fcd977e8540c7560c4dfae311401
Huy Vu <khanhhuy.dev@gmail.com>
2021-11-05 10:25:06 +0700

    Update packages/date-parser/src/helpers/weekdayIdxFromLuxon.js
    Co-authored-by: datbth <scott.bui@holistics.io>

--- packages/date-parser/src/helpers/weekdayIdxFromLuxon.js
+++ packages/date-parser/src/helpers/weekdayIdxFromLuxon.js
@@ -1,6 +1,6 @@
 const weekdayIdxFromLuxon = (luxon) => {
   const idx = luxon.weekday;
-  return idx === 7 ? 0 : idx; // our Sunday starts at 0, while Luxon is 7
+  return idx === 7 ? 0 : idx; // our Sunday is 0, while Luxon is 7
 };
 
 export default weekdayIdxFromLuxon;
